9ba5c48fb9f24b49153d34a7d19999379e51cdbc
Husnain Arif <arifhunain478@.gmail.com>
2026-02-10 22:38:50 -0800

    second

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import './App.css'
 
 function App() {
   let [PositionX, setPositionX] = useState()
+  let [Sayno,setSayno]=useState("no")
   let [PositionY, setPositionY] = useState()
-  let [BoxX, setBoxX] = useState(0)
-  let [BoxY, setBoxY] = useState(0)
+  let [BoxX, setBoxX] = useState(600)
+  let [BoxY, setBoxY] = useState(350)
 
   let move = (e) => {
     let x = e.clientX;
     let y = e.clientY;
 
 
-    console.log("we are there", (Math.hypot((x - BoxX), (y - BoxY))));
+    console.log("we are there", x,y);
     if ((Math.hypot((x - BoxX), (y - BoxY))) < 130) {
+      setSayno(" No 😅")
 
-      let newBoxX = Math.floor(Math.random() * (693 + 1) + 261);
+      let newBoxX = Math.floor(Math.random() * (988 + 1) + 140);
       let newBoxY = Math.floor(Math.random() * (370 + 1) + 80);
 
       setBoxX(newBoxX);
@@ -41,16 +43,16 @@ function App() {
 
       <div onMouseMove={(t) => move(t)} className=' bg-[#db4367] flex justify-center items-center flex-col  w-full h-screen'>
 
-        <h1 className="text-4xl md:text-5xl   font-bold text-white mb-10 text-center drop-shadow-lg">
+        <h1 className="text-4xl md:text-5xl pb-6  font-bold text-white mb-10 text-center drop-shadow-lg">
           💖 Will You Be My Valentine? 💖
         </h1>
-          <button className="px-8 py-3 bg-white text-rose-500 font-semibold text-lg rounded-full shadow-lg hover:scale-110 transition-transform duration-300">
+          <button className="px-8 py-3 bg-white text-rose-500 font-semibold pt-3 text-lg rounded-full shadow-lg hover:scale-110 transition-transform duration-300">
     Yes 💘
   </button>
 
 
-        <div className='bg-rose-600 text-white flex items-center justify-center w-[100px] h-[100px] rounded-full shadow-xl font-semibold cursor-pointer' style={{ position: "absolute", transition: "top 0.75s ease, left 0.75s ease", top: `${PositionY}px`, left: `${PositionX}px` }}>
-          No 😅
+        <div className='bg-rose-400 text-white flex   items-center px-8 py-3 font-bold justify-center  rounded-full shadow-xl font-semibold cursor-pointer' style={{ position: "absolute", transition: "top 0.75s ease, left 0.75s ease", top: `${PositionY}px`, left: `${PositionX}px` }}>
+          {Sayno}
 
         </div>
 
